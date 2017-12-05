8802c51b8fd0e0aa03268c82e8cb63e9d5648015
German <german2506.gs@gmail.com>
2017-12-05 06:23:44 -0700

    Se muestra lista
    Se ve la lista con nombres y fotos en un recyclerview

---
+++ app/src/main/java/com/example/eduardo/paselistar/ListaAlumnosAdaptador.java
@@ -0,0 +1,74 @@
+package com.example.eduardo.paselistar;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.eduardo.paselistar.modelos.AlumnosItem;
+
+import java.util.ArrayList;
+
+/**
+ * Created by eduardo on 5/12/17.
+ */
+
+public class ListaAlumnosAdaptador extends RecyclerView.Adapter<ListaAlumnosAdaptador.ViewHolder> {
+    private ArrayList<AlumnosItem> dataset;
+    private Context context;
+
+    public ListaAlumnosAdaptador(Context context){
+        this.context = context;
+        dataset = new ArrayList<>();
+    }
+
+    public void agregarListaAlumnos(ArrayList<AlumnosItem> alummnos){
+        dataset.addAll(alummnos);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.objeto_alumno,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position){
+        AlumnosItem a = dataset.get(position);
+        holder.nombreTextView.setText(a.getApellidopaterno()+" "+a.getApellidomaterno()+" "+a.getNombre());
+        Glide.with(context)
+                .load("http://189.202.197.43/fotos/"+a.getNcontrol()+".jpg")
+        .centerCrop()
+        .crossFade()
+        .diskCacheStrategy(DiskCacheStrategy.ALL)
+        .into(holder.fotoImageView);
+
+    }
+
+    @Override
+    public int getItemCount(){
+        return dataset.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView fotoImageView;
+        private TextView nombreTextView;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+
+            fotoImageView = (ImageView) itemView.findViewById(R.id.imageView);
+            nombreTextView = (TextView) itemView.findViewById(R.id.tvNombre);
+        }
+}
+
+}
--- app/src/main/java/com/example/eduardo/paselistar/MainActivity.java
+++ app/src/main/java/com/example/eduardo/paselistar/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.eduardo.paselistar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.example.eduardo.paselistar.apiPaseLista.PaseListaApiServicio;
@@ -9,6 +11,7 @@ import com.example.eduardo.paselistar.modelos.AlumnosItem;
 import com.example.eduardo.paselistar.modelos.AlumnosRespuesta;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,30 +21,47 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
     private Retrofit retrofit;
+
+    private RecyclerView recyclerView;
+    private ListaAlumnosAdaptador listaAlumnosAdaptador;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        listaAlumnosAdaptador = new ListaAlumnosAdaptador(this);
+        recyclerView.setAdapter(listaAlumnosAdaptador);
+        recyclerView.setHasFixedSize(true);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 1);
+        recyclerView.setLayoutManager(layoutManager);
+
         retrofit = new Retrofit.Builder()
                 .baseUrl("http://itculiacan.edu.mx/dadm/apipaselista/data/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         obtenerDatos();
     }
+
     private void obtenerDatos(){
         PaseListaApiServicio servicio = retrofit.create(PaseListaApiServicio.class);
         final Call<AlumnosRespuesta> listaAlumnosRespuesta = servicio.obtenerListaAlumnos();
+
         listaAlumnosRespuesta.enqueue(new Callback<AlumnosRespuesta>() {
             @Override
             public void onResponse(Call<AlumnosRespuesta> call, Response<AlumnosRespuesta> response) {
                 if(response.isSuccessful()){
                     AlumnosRespuesta listaAlumnos = response.body();
                     ArrayList<AlumnosItem> alumnos = listaAlumnos.getAlumnos();
-                    for (int i = 0; i<alumnos.size(); i++){
-                        AlumnosItem a = alumnos.get(i);
-                        Log.i(TAG, "alumno "+a.getNombre());
-                    }
+
+                    listaAlumnosAdaptador.agregarListaAlumnos(alumnos);
+
                 }else {
                     Log.e(TAG, " onResponse "+response.errorBody());
                 }
